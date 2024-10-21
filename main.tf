36959be4813f3abda0eeeae74b0ca1cd93274e34
satishranjan7183 <143581126+satishranjan7183@users.noreply.github.com>
2024-10-21 20:28:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.6.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+  subscription_id = "38d52de4-2fb6-4a85-96fb-5ba22d363e4e"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "ranjan" {
+  name     = "ranjan2"
+  location = "West Europe"
+}
